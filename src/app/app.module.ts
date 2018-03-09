021aff32cee1b5226ab43e93f8399e2f0a172b02
Rodzman <designrsp@gmail.com>
2018-03-09 01:08:02 -0300

    save tripsorter files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ResultsComponent } from './results/results.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsItemComponent } from './results/results-item/results-item.component';
+import { TripService } from './trip.service';
+import { RouterModule, Routes } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
+import { FilterPipe} from './search/filter.pipe';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ResultsComponent,
+    SearchComponent,
+    ResultsItemComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpModule,
+    JsonpModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+    )
   ],
-  providers: [],
+  providers: [TripService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
